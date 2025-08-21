a2d44d92fe99ede30563a4042bdddebf7550318d
April Rivenend <161380414+HarvestSorts@users.noreply.github.com>
2025-08-21 14:56:11 -0300

    print at the top to avoid console spam

--- public/twt/unixtoreadable.js
+++ public/twt/unixtoreadable.js
@@ -1,3 +1,9 @@
+      // print to console on page load
+        window.addEventListener('load', function() {
+        console.log('Loaded wintrmoon\'s UNIX to human readable script. This message is brought to you by April Rivenend.');
+        console.warn('Load this script as async (<script src="file.js" async>) to load it from an external file. This restriction is not required if you are loading the script in-line!');
+      });
+      
       //NOTE - converts the unix to a readable time stamp
         document.querySelectorAll("time.post-time").forEach(el => {
           let timestamp = parseInt(el.getAttribute("datetime"), 10);
@@ -12,12 +18,6 @@
           minute: '2-digit',
           hour12: true
         });
-
-        // print to console on page load
-        window.addEventListener('load', function() {
-        console.log('Loaded wintrmoon\'s UNIX to human readable script. This message is brought to you by April Rivenend.');
-        console.warn('Load this script as async (<script src="file.js" async>) to load it from an external file. This restriction is not required if you are loading the script in-line!');
-      });
         
       //NOTE - this is so that the unix time becomes machine readable, you can remove this line if you want but then the html gods wont like you
         el.setAttribute("datetime", date.toISOString());
